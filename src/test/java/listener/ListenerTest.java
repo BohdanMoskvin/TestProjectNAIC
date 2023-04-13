1e3804ace781d0aae1bbd9cba8030cab0c5dc088
BohdanMoskvin <mskbohdan@gmail.com>
2023-04-13 09:23:26 +0300

    Updated pom file

--- src/test/java/listener/ListenerTest.java
+++ src/test/java/listener/ListenerTest.java
@@ -1,9 +1,9 @@
 package listener;
 
 import io.qameta.allure.Attachment;
-import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
-import org.apache.commons.io.FileUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,62 +12,64 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import tests.TestInit;
 
-import java.io.File;
-
 
 @Log4j
 public class ListenerTest implements ITestListener {
+    Logger logger = LogManager.getLogger(ListenerTest.class);
 
-    @Override
-    public void onFinish(ITestContext result) {
-        log.info("  The name of the testcase  is On Finish :-" + result.getName() + "             ");
-        log.info("=========================================");
-    }
-
-
-    @Override
-    public void onStart(ITestContext result) {
-        log.info("The name of the testcase on Start" + result.getName() + "            ");
-        log.info("===============================================================");
-    }
-
-
-    // When Test case get failed, this method is called.
     @Override
     public void onTestFailure(ITestResult result) {
+        logger.info("---------------------------------------------------------------");
+        logger.info("Failed because of - " + result.getThrowable());
+        logger.info("---------------------------------------------------------------");
         WebDriver driver = ((TestInit) result.getInstance()).getDriver();
-        screenShot(driver);
-        log.info("The name of the testcase failed is :" + result.getName());
-        log.info("===============================================================");
-
+        if (driver != null) {
+            captureScreen(driver);
+        }
     }
 
-    // When Test case get Skipped, this method is called.
     @Override
     public void onTestSkipped(ITestResult result) {
+        logger.info("---------------------------------------------------------------");
+        logger.info("Skipped because of - " + result.getThrowable());
+        logger.info("---------------------------------------------------------------");
         WebDriver driver = ((TestInit) result.getInstance()).getDriver();
-        screenShot(driver);
-        log.info("The name of the testcase Skipped is :" + result.getName());
-        log.info("===============================================================");
+        if (driver != null) {
+            captureScreen(driver);
+        }
     }
 
-    // When Test case get Started, this method is called.
     @Override
     public void onTestStart(ITestResult result) {
-        log.info(result.getName() + " test case started");
-        log.info("===============================================================");
+        logger.info("---------------------------------------------------------------");
+        logger.info(result.getMethod().getMethodName() + " Started");
+        logger.info("---------------------------------------------------------------");
+        logger.info(result.getMethod().getDescription());
     }
 
-    // When Test case get passed, this method is called.
     @Override
     public void onTestSuccess(ITestResult result) {
-        log.info("The name of the testcase passed is :" + result.getName());
+        logger.info("---------------------------------------------------------------");
+        logger.info(result.getMethod().getMethodName() + " Passed");
+        logger.info("---------------------------------------------------------------");
     }
 
-    @SneakyThrows
-    public void screenShot(WebDriver driver) {
-        TakesScreenshot screenshot = (TakesScreenshot) driver;
-        File source = screenshot.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File(String.format("target/screenshot/screenshot%s.png", System.currentTimeMillis())));
+    @Override
+    public void onStart(ITestContext context) {
+        logger.info("===============================================================");
+        logger.info("     On Start :-" + context.getName() + "                      ");
+        logger.info("===============================================================");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        logger.info("===============================================================");
+        logger.info("     On Finish :-" + context.getName() + "                     ");
+        logger.info("===============================================================");
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    private byte[] captureScreen(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
